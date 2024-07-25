f899b358f74958081f80369833cfe53190ecd883
AishuPola <aishwarya.pola@proclink.com>
2024-07-25 17:26:04 +0530

    Movie list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { ColorGameComponent } from './color-game/color-game.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MovielistComponent } from './movielist/movielist.component';
 export const routes: Routes = [
   {
     path: '',
-    component: MoviesComponent,
+    component: MovielistComponent,
   },
   {
     path: 'color-game',
--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -13,7 +13,7 @@ export class MoviesComponent {
   @Input() name = 'Vikram';
   @Input() poster =
     'https://m.media-amazon.com/images/M/MV5BMmJhYTYxMGEtNjQ5NS00MWZiLWEwN2ItYjJmMWE2YTU1YWYxXkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg';
-  @Input() rating = '';
+  @Input() rating = 0;
   @Input() summary = '';
 
   show = true;
